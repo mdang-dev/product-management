2088b4500a79140e5605e07ad6df42f63e106271
mdang-dev <minhdang25.dev@gmail.com>
2025-03-22 12:38:40 +0700

    handle router authentication

--- backend/src/main/java/com/product/app/security/SecurityConfig.java
+++ backend/src/main/java/com/product/app/security/SecurityConfig.java
@@ -15,8 +15,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -47,7 +45,7 @@ public class SecurityConfig {
         this.userDetailsService = userDetailsService;
     }
 
-    private final String[] ACCESS_ENDPOINTS = {"/api/auth/**", "/users/logout"};
+    private final String[] ACCESS_ENDPOINTS = {"/api/auth/**"};
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -81,11 +79,6 @@ public class SecurityConfig {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
         return configuration.getAuthenticationManager();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
     }
 
 }
