2da98a7dceb4521ec5bd59e77c05bcc4af4fa485
mdang-dev <minhdang25.dev@gmail.com>
2025-03-25 15:32:47 +0700

    api for crud products

--- backend/src/main/java/com/product/app/service/ProductService.java
+++ backend/src/main/java/com/product/app/service/ProductService.java
@@ -1,4 +1,30 @@
 package com.product.app.service;
 
+import com.product.app.model.Product;
+import com.product.app.model.User;
+import com.product.app.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> getAllProducts() {
+        return  productRepository.findAll();
+    }
+
+    public Product createOrUpdateProduct(Product product){
+        return productRepository.save(product);
+    }
+
+    public void deleteProduct(String id){
+        productRepository.delete(productRepository.findById(id).
+                orElseThrow(() -> new RuntimeException("Product Not Found !")));
+    }
+
 }
