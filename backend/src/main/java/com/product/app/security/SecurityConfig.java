3e40b937c2fd86247cebb0d7e9abe8c9f4904435
mdang-dev <minhdang25.dev@gmail.com>
2025-03-26 15:38:13 +0700

    custom config security

--- backend/src/main/java/com/product/app/security/SecurityConfig.java
+++ backend/src/main/java/com/product/app/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
@@ -48,12 +49,13 @@ public class SecurityConfig {
     }
 
     private final String[] ACCESS_ENDPOINTS = {"/api/auth/**", "/users/logout", "/products/**"};
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http.csrf(AbstractHttpConfigurer::disable)
                 .cors(Customizer.withDefaults())
-                .authorizeHttpRequests(authorize -> authorize.requestMatchers(ACCESS_ENDPOINTS).permitAll()
+                .authorizeHttpRequests(authorize ->
+                                authorize.requestMatchers(HttpMethod.GET, "/api/products").permitAll()
+                        .requestMatchers(ACCESS_ENDPOINTS).permitAll()
                         .anyRequest().authenticated()
                 ).userDetailsService(userDetailsService)
                 .exceptionHandling(ex -> ex.accessDeniedHandler(((request, response, accessDeniedException) -> response.setStatus(403)))
